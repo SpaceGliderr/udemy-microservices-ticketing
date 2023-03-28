8c65d9d7078e0f8d5e5f3a8693aae269b07e1c09
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2023-03-28 14:44:16 +0800

    Add tickets service and routes

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,9 +5,14 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import {
+  currentUser,
   errorHandler,
   NotFoundError,
 } from "@nickiii-microservices-course/common";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 // Tells express that requests will be coming in from a proxy (ingress nginx)
@@ -21,6 +26,12 @@ app.use(
     secure: process.env.NODE_ENV !== "test", // Make sure cookies are sent via HTTPS
   })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
