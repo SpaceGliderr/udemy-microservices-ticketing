4dae0d7fe4ece1b5a8e3e459731bda6519db76a2
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2023-03-28 14:44:51 +0800

    Add additional setup to tickets testing

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,10 +1,9 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
-import request from "supertest";
-import { app } from "../app";
+import jwt from "jsonwebtoken";
 
 declare global {
-  var signup: () => Promise<string[]>;
+  var signup: () => string[];
 }
 
 let mongo: any;
@@ -30,14 +29,30 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signup = async () => {
-  const email = "test@test.com";
-  const password = "password";
+global.signup = () => {
+  // Can't reach over to Auth service to send a request
+  // Don't want to introduce inter-service dependency
+  // Tests must be self-contained and independent
+  // Need to create a fake JWT and Cookie object
 
-  const response = await request(app)
-    .post("/api/users/signup")
-    .send({ email, password })
-    .expect(201);
+  // Build a JWT object
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+  };
 
-  return response.get("Set-Cookie");
+  // Create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session Object
+  const session = { jwt: token };
+
+  // Turn session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Encode JSON as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // Build string with encoded data
+  return [`session=${base64}`];
 };
