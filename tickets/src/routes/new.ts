d780fd5eb2c3bf66f5b808283ee97f9d851951d7
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2023-04-15 18:17:00 +0800

    Publish ticket created and updated event in new and update routes

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -4,7 +4,9 @@ import {
 } from "@nickiii-microservices-course/common";
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
 import { Ticket } from "../models/ticket";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -23,6 +25,13 @@ router.post(
 
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
     await ticket.save();
+
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }
